Droid-VM/Virtualization

[rkp] Generate EC P-256 key pair for the RKP HAL
This cl generates the EC P-256 key pair needed by the RKP HAL.
The MAC of the public key and wrapping of the private key will be
handled in subsequent cls.

Bug: 299055662
Test: atest rialto_test
Change-Id: I253130e62bbf70cb5a7abe1a056fc4f77d30aa6e

rialto/src/requests/mod.rs
@@ -15,6 +15,7 @@
 //! This module contains functions for the request processing.
 
 mod api;
+mod keys;
 mod rkp;
 
 pub use api::process_request;

Project-Mist-OS/packages_modules_Virtualization

rialto/src/requests/mod.rs
@@ -15,6 +15,7 @@
 //! This module contains functions for the request processing.
 
 mod api;
+mod ec_key;
 mod rkp;
 
 pub use api::process_request;